5afc96f31c9f1b5d2ff9a7221efd0cf4c68ed289
rafelis1997 <rafaelheros80@gmail.com>
2024-03-12 18:41:25 -0300

    fix: path errors

--- src/domain/wallet/application/errors/document-is-invalid.ts
+++ src/domain/wallet/application/errors/document-is-invalid.ts
@@ -1,4 +1,4 @@
-import { UseCaseError } from '../../../core/errors/use-case-error'
+import { UseCaseError } from '@/core/errors/use-case-error'
 
 export class DocumentIsInvalidError extends Error implements UseCaseError {
   constructor(identifier: string) {
