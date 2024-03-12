485512c8cf371edb4d752e3fa5729a7fa1375724
rafelis1997 <rafaelheros80@gmail.com>
2024-03-12 18:41:15 -0300

    feat: add http folder and modules

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config'
 import { EnvModule } from './env/env.module'
 import { EnvService } from './env/env.service'
 import { EventsModule } from './events/events.module'
+import { HttpModule } from './http/http.module'
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { EventsModule } from './events/events.module'
     }),
     EnvModule,
     EventsModule,
+    HttpModule,
   ],
   providers: [EnvService],
 })
